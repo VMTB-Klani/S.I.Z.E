2a177fc993198593861a1aeb91916dfeaa7b2789
pattotherick <patrickherfurth@yahoo.de>
2018-12-03 14:53:01 +0100

    add inteactalbeobj

---
+++ S.I.Z.E/Assets/Scripts/Inteactablegameobj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inteactablegameobj : MonoBehaviour {
+
+	[SerializeField]
+	string m_Describtion;
+	public void start()
+	{
+
+	}
+	public virtual string OnViewPoint()
+	{
+		return m_Describtion;
+	}
+	public virtual void OnSelcetedObj()
+	{
+	}
+}
---
+++ S.I.Z.E/Assets/Scripts/key.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : Inteactablegameobj {
+
+	public override void OnSelcetedObj()
+	{
+		Destroy(this.gameObject);
+	}
+}
