80a4a5f0ffef53881219ef5de70ae509d7fa56ee
Hegolin <45560799+Hegolin@users.noreply.github.com>
2018-12-04 13:40:27 +0100

    Added LevelExit-Script

---
+++ S.I.Z.E/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Collision has been detected...");
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided with player, loading next scene.");
+            LoadNextScene();
+        }
+    }
+
+    /// <summary>
+    /// Load the next scene in the build through it's index.
+    /// </summary>
+    public void LoadNextScene()
+    {
+        Debug.Log("Get current Index...");
+        int currentsceneindex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("Get next scene...");
+        SceneManager.LoadScene(currentsceneindex + 1);
+    }
+}
