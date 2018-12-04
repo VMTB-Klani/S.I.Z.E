93fda6f500ccd816f626f0ff224ccb3c861eb3e7
Hegolin <45560799+Hegolin@users.noreply.github.com>
2018-12-04 17:05:40 +0100

    removed menu stuff from gamemanager, added menu functionalities

--- S.I.Z.E/Assets/Scripts/GameManager.cs
+++ S.I.Z.E/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 /// <summary>
 /// decides when the game is won or lost
 /// pauses the game when asked
@@ -52,30 +53,10 @@ public class GameManager : MonoBehaviour
         playerPrefab = Instantiate(playerPrefab, player_Spawnpoint);
     }
 
-    void Update()
-    {
-        ///if game not started, skip rest below
-        if (!hasGameStarted)
-        {
-            return;
-        }
-
-        ///get player stuff
-        //if (r_player == null)
-        //{
-        //    r_player = FindObjectOfType<Player>();
-        //}
-
-        ///if ESC pressed, toggle the cursor lock
-        ///which triggers the pause menu when the player is alive
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            //if (r_player.gameObject.activeInHierarchy)
-            {
-                ShowMenu();
-            }
-        }
-    }
+void Update()
+{
+    
+}
 
     /// <summary>
     /// Lock the cursor and hide it
@@ -94,42 +75,5 @@ public class GameManager : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
-    }
-
-    /// <summary>
-    /// shows and hides the pause menu
-    /// </summary>
-    public void ShowMenu()
-    {
-        isGamePaused = !isGamePaused;
-
-        if (isGamePaused)
-        {
-            PauseGame();
-        }
-        else
-        {
-            ResumeGame();
-        }
-    }
-
-    /// <summary>
-    /// resumes the game
-    /// </summary>
-    void ResumeGame()
-    {
-        isGamePaused = false;
-
-        Time.timeScale = 1;
-    }
-
-    /// <summary>
-    /// pauses the game
-    /// </summary>
-    void PauseGame()
-    {
-        isGamePaused = true;
-
-        Time.timeScale = 0;
     }
 }
---
+++ S.I.Z.E/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+    bool isGamePaused = false;
+
+    public GameObject m_UIPauseMenu;
+
+
+    void Update()
+    {
+        ///if ESC pressed, open PauseMenu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowMenu();
+        }
+    }
+
+    /// <summary>
+    /// resumes the game
+    /// </summary>
+    public void ResumeGame()
+    {
+        isGamePaused = false;
+
+        m_UIPauseMenu.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+
+    /// <summary>
+    /// pauses the game
+    /// </summary>
+    void PauseGame()
+    {
+        isGamePaused = true;
+
+        m_UIPauseMenu.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+
+    /// <summary>
+    /// shows and hides the pause menu
+    /// </summary>
+    public void ShowMenu()
+    {
+        isGamePaused = !isGamePaused;
+
+        if (isGamePaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+}
