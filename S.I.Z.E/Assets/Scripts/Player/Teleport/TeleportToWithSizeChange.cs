61150f5bf30d8997f0302e70946c57dfe0bb52d3
pattotherick <patrickherfurth@yahoo.de>
2018-12-05 14:32:43 +0100

    level 2 edit

---
+++ S.I.Z.E/Assets/Scripts/Player/Teleport/TeleportToWithSizeChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToWithSizeChange : TeleportTo {
+
+	public override void OnTriggerEnter(Collider _other)
+	{
+		base.OnTriggerEnter(_other);
+		if (_other.gameObject.tag == "Player")
+		{
+			_other.gameObject.transform.localScale = new Vector3(1, 1, 1);
+		}
+	}
+}
